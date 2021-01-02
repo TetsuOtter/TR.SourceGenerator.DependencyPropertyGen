685c20bb22741f69b1dba6e601607242ce836e53
ＴＲ <public@technotter.com>
2021-01-03 01:56:42 +0900

    Attributeをコメントアウト
    テスト用に記述方法の差異も作成

--- TR.SourceGenerator.DependencyPropertyGen.Sample/SampleControl_Inherit.xaml.cs
+++ TR.SourceGenerator.DependencyPropertyGen.Sample/SampleControl_Inherit.xaml.cs
@@ -3,8 +3,8 @@ using System.Windows.Media;
 
 namespace TR.SourceGenerator.DependencyPropertyGen.Sample
 {
-	[DependencyPropertyGen(typeof(Brush), "FrameBrush")]
-	[DependencyPropertyGen(typeof(Thickness), "FrameThickness")]
+	//[DependencyPropertyGen(typeof(Brush), "FrameBrush")]
+	//[DependencyPropertyGenAttribute(typeof(Thickness), "FrameThickness")]
 	public partial class SampleControl_Inherit : SampleControl_Base //must set "partial" keyword
 	{
 		static SampleControl_Inherit() => DefaultStyleKeyProperty.OverrideMetadata(typeof(SampleControl_Inherit), new FrameworkPropertyMetadata(typeof(SampleControl_Inherit)));
