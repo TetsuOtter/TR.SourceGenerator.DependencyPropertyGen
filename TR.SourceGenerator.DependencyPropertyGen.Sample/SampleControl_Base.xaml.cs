f41cdf9301fbb9a751b6b09c59c36031197040cc
ＴＲ <public@technotter.com>
2021-01-02 18:23:10 +0900

    サンプルプロジェクトに基礎要素を追加, Analyzer参照を追加

---
+++ TR.SourceGenerator.DependencyPropertyGen.Sample/SampleControl_Base.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+using TR.SourceGenerator;
+
+namespace TR.SourceGenerator.DependencyPropertyGen.Sample
+{
+	[DependencyPropertyGen(typeof(string), "MyText")]
+	[DependencyPropertyGen(typeof(Brush), "MyTextColor")]
+	public partial class SampleControl_Base : Control //must set "partial" keyword
+	{
+		static SampleControl_Base() => DefaultStyleKeyProperty.OverrideMetadata(typeof(SampleControl_Base), new FrameworkPropertyMetadata(typeof(SampleControl_Base)));
+		public SampleControl_Base()
+		{
+			MyText = "abc";
+			MyTextColor = Brushes.White;
+		}
+	}
+}
