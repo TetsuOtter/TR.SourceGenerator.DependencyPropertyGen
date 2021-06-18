74c1a9cb5b9f874f97f001f9994967f61994f559
ＴＲ <public@technotter.com>
2021-06-18 09:02:44 +0900

    ReadOnly実装に伴うサンプル実装追加

--- TR.SourceGenerator.DependencyPropertyGen.Sample/SampleControl_Base.xaml.cs
+++ TR.SourceGenerator.DependencyPropertyGen.Sample/SampleControl_Base.xaml.cs
@@ -6,8 +6,11 @@ namespace TR.SourceGenerator.DependencyPropertyGen.Sample
 {
 	[DependencyPropertyGen(typeof(string), "MyText")]
 	[DependencyPropertyGen(typeof(Brush), "MyTextColor")]
+	[DependencyPropertyGen(typeof(double), "MyFontSize", null, true, "private")]
 	public partial class SampleControl_Base : Control //must set "partial" keyword
 	{
 		static SampleControl_Base() => DefaultStyleKeyProperty.OverrideMetadata(typeof(SampleControl_Base), new FrameworkPropertyMetadata(typeof(SampleControl_Base)));
+
+		public SampleControl_Base() => MyFontSize = 16;//You can change value from here
 	}
 }
--- TR.SourceGenerator.DependencyPropertyGen.Sample/SampleControl_Inherit.xaml.cs
+++ TR.SourceGenerator.DependencyPropertyGen.Sample/SampleControl_Inherit.xaml.cs
@@ -8,5 +8,6 @@ namespace TR.SourceGenerator.DependencyPropertyGen.Sample
 	public partial class SampleControl_Inherit : SampleControl_Base //must set "partial" keyword
 	{
 		static SampleControl_Inherit() => DefaultStyleKeyProperty.OverrideMetadata(typeof(SampleControl_Inherit), new FrameworkPropertyMetadata(typeof(SampleControl_Inherit)));
+		//void abc() => MyFontSize = 10;// Cannot Access from here
 	}
 }
