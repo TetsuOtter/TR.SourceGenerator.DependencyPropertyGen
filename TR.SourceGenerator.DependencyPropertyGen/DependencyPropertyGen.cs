e74b468a8778949adb8cebd8036e2bcae1a50d55
ＴＲ <public@technotter.com>
2021-06-22 04:55:39 +0900

    不要なusingを削除

--- TR.SourceGenerator.DependencyPropertyGen/DependencyPropertyGen.cs
+++ TR.SourceGenerator.DependencyPropertyGen/DependencyPropertyGen.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Text;
 
 namespace TR.SourceGenerator
 {
